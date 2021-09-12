8f926df4270eec984bdc2243eb46aff23ec4fa82
dholi <comunidade@dholi.dev>
2021-09-12 00:40:49 -0300

    V 1.0.1

--- src/module-payu/Model/InstantPurchase/CreditCard/TokenFormatter.php
+++ src/module-payu/Model/InstantPurchase/CreditCard/TokenFormatter.php
@@ -3,7 +3,7 @@
 * 
 * PayU Latam para Magento 2
 * 
-* @category     Ã©lOOm
+* @category     elOOm
 * @package      Modulo PayU Latam
 * @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
 * @version      1.0.1
