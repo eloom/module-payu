7ed9421de31c86d6096fa9fd70efd2d8ca0ae8ec
dholi <comunidade@dholi.dev>
2021-10-01 16:05:09 -0300

    Docs

---
+++ src/module-payu/Block/Adminhtml/Config/Fieldset/DocsHint.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* 
+* PayU Latam para Magento 2
+* 
+* @category     elOOm
+* @package      Modulo PayU Latam
+* @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
+* @version      1.0.1
+* @license      https://eloom.tech/license
+*
+*/
+declare(strict_types=1);
+
+namespace Eloom\PayU\Block\Adminhtml\Config\Fieldset;
+
+use Magento\Backend\Block\Template;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
+
+class DocsHint extends Template implements RendererInterface {
+	
+	/**
+	 * @param AbstractElement $element
+	 * @return string
+	 */
+	public function render(AbstractElement $element) {
+		$html = '';
+		
+		$html .= sprintf('<tr id="row_%s">', $element->getHtmlId());
+		$html .= '<td class="label">'. __($element->getLabel()) .'</td>';
+		$html .= '<td class="value">';
+		$html .= sprintf(
+			'<a href="%s" target="_blank">' . __('Documentation') . '</a>',
+			'https://docs.eloom.tech/payment/payu-latam'
+		);
+		$html .= '</td></tr>';
+		
+		return $html;
+	}
+}
---
+++ src/module-payu/Block/Adminhtml/Config/Fieldset/VersionHint.php
@@ -0,0 +1,54 @@
+<?php
+/**
+* 
+* PayU Latam para Magento 2
+* 
+* @category     elOOm
+* @package      Modulo PayU Latam
+* @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
+* @version      1.0.1
+* @license      https://eloom.tech/license
+*
+*/
+declare(strict_types=1);
+
+namespace Eloom\PayU\Block\Adminhtml\Config\Fieldset;
+
+use Magento\Backend\Block\Template;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
+use Magento\Framework\Module\ModuleResource;
+
+class VersionHint extends Template implements RendererInterface {
+	
+	/**
+	 * @var ModuleResource
+	 */
+	private $moduleResource;
+	
+	/**
+	 * @param ModuleResource $moduleResource
+	 * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+	 * @SuppressWarnings(Magento.TypeDuplication)
+	 */
+	public function __construct(ModuleResource $moduleResource) {
+		$this->moduleResource = $moduleResource;
+	}
+	
+	/**
+	 * @param AbstractElement $element
+	 * @return string
+	 */
+	public function render(AbstractElement $element) {
+		$dbVersion = (string)$this->moduleResource->getDbVersion('Eloom_PayU');
+		
+		$version = '<strong>' . $dbVersion . '</strong> - [<a href="https://github.com/eloom/module-payu/releases">' . __('Releases') . '</a>]';
+		
+		$html = '';
+		$html .= sprintf('<tr id="row_%s">', $element->getHtmlId());
+		$html .= '<td class="label">' . __($element->getLabel()) . '</td>';
+		$html .= '<td>' . $version . '</td></tr>';
+		
+		return $html;
+	}
+}
